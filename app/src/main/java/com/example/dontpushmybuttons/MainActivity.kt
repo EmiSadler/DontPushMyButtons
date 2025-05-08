ef59ae2650bf4c048f9ea3ec251ffa548f318e31
ImogenLovell <imogen.lovell@yahoo.co.uk>
2025-05-08 14:16:30 +0100

    Added triangles and hexagons

--- app/src/main/java/com/example/dontpushmybuttons/MainActivity.kt
+++ app/src/main/java/com/example/dontpushmybuttons/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dontpushmybuttons
 
+import android.graphics.Path
 import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,6 +10,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.GenericShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,6 +20,14 @@ import com.example.dontpushmybuttons.ui.theme.DontPushMyButtonsTheme
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.withTransform
+import androidx.core.graphics.rotationMatrix
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.toPath
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -46,13 +56,18 @@ class ButtonItem(
 val button1 = ButtonItem(
     label = 1,
     color = Color.Blue,
-    shape = RoundedCornerShape(0.dp)
+    shape = GenericShape { size, _ ->
+        addPath(createSmoothTrianglePath(size.maxDimension))
+    }
+
 )
 
 val button2 = ButtonItem(
     label = 2,
     color = Color.Red,
-    shape = RoundedCornerShape(16.dp)
+    shape = GenericShape { size, _ ->
+        addPath(createSmoothHexagonPath(size.minDimension))
+    }
 )
 
 val button3 = ButtonItem(
@@ -119,3 +134,28 @@ fun PreviewFixedGrid() {
         FixedGrid()
     }
 }
+
+
+fun createSmoothHexagonPath(size: Float): androidx.compose.ui.graphics.Path {
+    val roundedPolygon = RoundedPolygon(
+        numVertices = 6,
+        radius = size / 1.9f,
+        centerX = size / 2,
+        centerY = size / 2,
+        rounding = CornerRounding(size / 13f, smoothing = 0.1f)
+    )
+    return roundedPolygon.toPath().asComposePath()
+}
+
+fun createSmoothTrianglePath(size: Float): androidx.compose.ui.graphics.Path {
+    val roundedTriangle = RoundedPolygon(
+        numVertices = 3,
+        radius = size / 1.6f,
+        centerX = size / 2,
+        centerY = size / 2,
+        rounding = CornerRounding(size / 10f, smoothing = 0.1f)
+    )
+
+    return roundedTriangle.toPath().asComposePath()
+}
+

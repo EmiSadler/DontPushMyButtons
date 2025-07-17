f899d86daf908c339d4343f5fae0a2726091fb39
Emily Sadler <emily@sadler.gg>
2025-07-17 09:45:33 +0100

    sorted bug that was preventing compiling

--- app/src/main/java/com/example/dontpushmybuttons/MainActivity.kt
+++ app/src/main/java/com/example/dontpushmybuttons/MainActivity.kt
@@ -3,50 +3,42 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.GenericShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.foundation.Image
-import androidx.compose.ui.res.painterResource
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.material3.Switch
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.asComposePath
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.graphics.toColorInt
-import androidx.graphics.shapes.CornerRounding
-import androidx.graphics.shapes.RoundedPolygon
-import androidx.graphics.shapes.toPath
 import com.example.dontpushmybuttons.ui.theme.DontPushMyButtonsTheme
 
 
@@ -121,22 +113,13 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-        override fun onDestroy() {
-            super.onDestroy()
-            soundManager.release()
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        soundManager.release()
+    }
 }
 
 
-
-@Composable
-fun Title(label: String) {
-        Text(
-            text = label,
-            style = MaterialTheme.typography.headlineMedium,
-            modifier = Modifier.padding(16.dp)
-        )
-    }
 
 @Composable
 fun FixedGrid(
@@ -367,6 +350,3 @@ fun FixedGrid(
                 .padding(vertical = 0.dp),
         )
     }
-
-
-
